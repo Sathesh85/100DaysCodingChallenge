020ac83ddb0f92862be71b7341c1a0156e73fdb9
satheshkumarvijayakumar <satheshkumarvijayakumar@192.168.0.26>
2020-07-12 15:31:47 -0400

    Optimized solution for next largest number

--- src/com/challenge/stack/NextLargestNumber.java
+++ src/com/challenge/stack/NextLargestNumber.java
@@ -1,13 +1,14 @@
 package com.challenge.stack;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class NextLargestNumber {
-	
+
 	/*
 	 * 1 3 2 4
 	 * 
-	 * 1 
+	 * 1
 	 * 
 	 * 
 	 * O(n^2);
@@ -16,70 +17,58 @@ public class NextLargestNumber {
 	 * 
 	 * 
 	 */
-	
+
 	public static void main(String args[]) {
 		NextLargestNumber n = new NextLargestNumber();
-		int arr[] = {1,3,2,4};
+		int arr[] = { 1, 3, 2, 4 };
 		n.findNextLargestNumber(arr);
+		n.findNextLargetNumberOptimized(arr);
 		
-		int arr1[] = {4,3,2,1};
+
+		int arr1[] = { 4, 3, 2, 1 };
 		n.findNextLargestNumber(arr1);
+		n.findNextLargetNumberOptimized(arr1);
 	}
-	
-	
+
 	/*
 	 * Solved using n^2
 	 * 
 	 * 
 	 */
 	private void findNextLargestNumber(int arr[]) {
-		
-		for(int i =0; i< arr.length; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			boolean found = false;
-			for(int j = i+1; j < arr.length ; j++) {
-				if(arr[i] < arr[j]) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] < arr[j]) {
 					System.out.println(arr[i] + "<" + arr[j]);
 					found = true;
 					break;
 				}
 			}
-			if(!found) {
-				System.out.println(arr[i] + "<" +-1);
-			}
-		}
-		
-		
-	}
-	
-	private void findNextLargetNumberOptimized(int arr[]) {
-		Stack<Integer> s = new Stack<>();
-		s.add(arr[0]);
-		for(int a : arr) {
-			if(a>s.peek()) {
-				s.add(a);
-			}
-		}
-		
-		/*
-		 * 1 3 2 4
-		 * 
-		 *  4 3 1 
-		 * 
-		 */
-		
-		for(int i = arr.length-1; i>=0; i--) {
-			if(arr[i] > s.peek()) {
-				System.out.println(arr[i] + "<" + s.peek());
-			} else if(arr[i] == s.peek()){ 
-				s.pop();
-			}else {
+			if (!found) {
 				System.out.println(arr[i] + "<" + -1);
 			}
 		}
+	}
+
+	private void findNextLargetNumberOptimized(int arr[]) {
+		int res[] = new int[arr.length];
+		Arrays.fill(res, -1);
 		
+		Stack<Integer> s = new Stack<>();
+		
+		for(int i =0 ; i < arr.length; i++) {
+			
+			while (!s.isEmpty() && arr[i]> arr[s.peek()]) {
+				res[s.pop()] = arr[i];
+			}
+			s.push(i);
+		}
+		
+		for( int i =0 ; i < arr.length; i++) {
+			System.out.println(arr[i] +" next greatest element is " + res[i]);
+		}
 		
 	}
-	
-	
 
 }

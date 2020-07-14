4e34b2f28c8b555779c2ca691062be50bdb3dc16
satheshkumarvijayakumar <satheshkumarvijayakumar@192.168.0.26>
2020-07-13 21:16:06 -0400

    Math Max problem : Day #4

---
+++ src/com/challenge/tree/MaxPathSum.java
@@ -0,0 +1,24 @@
+package com.challenge.tree;
+
+public class MaxPathSum {
+	
+	int max = Integer.MIN_VALUE;
+
+	public int maxPathSum(Node root) {
+		
+		if(root == null)
+			return 0;
+		
+		int lSum = Math.max(maxPathSum(root.left),0);
+		
+		int rSum = Math.max(maxPathSum(root.right),0);
+		
+		int totalSum = lSum + rSum + root.data;
+		
+		max = Math.max(max, totalSum);
+		
+		return root.data + Math.max(lSum, rSum);
+		
+	}
+
+}

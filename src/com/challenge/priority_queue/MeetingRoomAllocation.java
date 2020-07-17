10b55cfbe58c157b25ce2964fb39c14f3a7faded
sathesh <satheshkumarv85@gmail.com>
2020-07-17 13:04:16 -0400

    Merge Interval problem

--- src/com/challenge/priority_queue/MeetingRoomAllocation.java
+++ src/com/challenge/priority_queue/MeetingRoomAllocation.java
@@ -1,37 +1,40 @@
 package com.challenge.priority_queue;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class MeetingRoomAllocation {
-	
+
 	public static void main(String[] args) {
-		int[][] intervals = {{0,30},{15,20},{5,10},{16,18},{20,25},{10,20}};
-		
+//		int[][] intervals = { { 5, 8 }, { 6, 8 } };
+		int[][] intervals = { { 0,30 }, { 15,20 }, {5,10} };
+
 		MeetingRoomAllocation m = new MeetingRoomAllocation();
-		
+
 		int minRoomRequired = m.minMeetingRooms(intervals);
 		System.out.println(minRoomRequired);
 	}
-	
-	 public int minMeetingRooms(int[][] intervals) {
-		 PriorityQueue<Integer> allocator = new PriorityQueue<>();
-		 
-		 
-		 Arrays.sort(intervals,  (int[] a, int[] b)-> a[0]-b[0]);
-		 
-		 allocator.add(intervals[0][1]);
-		 
-		 for(int i =1; i< intervals.length; i++) {
-			 
-			 if(allocator.peek()<intervals[i][1]) {
-				 allocator.poll();
-			 }
-			 allocator.add(intervals[i][1]);
-		 }
-		 
-		 return allocator.size();
-	 }
+
+	public int minMeetingRooms(int[][] intervals) {
+		if (intervals.length == 0) {
+			return 0;
+		}
+
+		PriorityQueue<Integer> allocator = new PriorityQueue<>();
+
+		Arrays.sort(intervals, (int[] a, int[] b) -> a[0] - b[0]);
+
+		allocator.add(intervals[0][1]);
+
+		for (int i = 1; i < intervals.length; i++) {
+
+			if (intervals[i][0] >= allocator.peek()) {
+				allocator.poll();
+			}
+			allocator.add(intervals[i][1]);
+		}
+
+		return allocator.size();
+	}
 
 }
---
+++ src/com/challenge/priority_queue/MergeIntervals.java
@@ -0,0 +1,52 @@
+package com.challenge.priority_queue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntervals {
+
+	public static void main(String[] args) {
+		int[][] intervals = { { 0, 30 }, { 5, 35 }, { 40, 50 } };
+
+		MergeIntervals m = new MergeIntervals();
+
+		int[][] mergeArr = m.merge(intervals);
+		System.out.println(mergeArr);
+	}
+
+	public int[][] merge(int[][] intervals) {
+
+		if(intervals.length == 0){
+            return intervals;
+        }
+        
+        Arrays.sort(intervals, (int[] a, int[] b) -> a[0] - b[0]);
+        
+
+		List<int[]> result = new ArrayList<>();
+
+		int e = intervals[0][1];
+		int s = intervals[0][0];
+		for (int i = 1; i < intervals.length; i++) {
+
+			if (intervals[i][0] <= e) {
+				if (intervals[i][1] >= e) {
+					e = intervals[i][1];
+				}
+			} else {
+				result.add(new int[] { s, e });
+				s = intervals[i][0];
+				e = intervals[i][1];
+			}
+		}
+		
+		if(s!=-1) {
+			result.add(new int[] { s, e });
+		}
+
+		return result.toArray(new int[result.size()][]);
+
+	}
+
+}

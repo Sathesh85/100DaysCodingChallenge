4e50b721bba1192fe35a3ed43258d023308e41a8
sathesh <satheshkumarv85@gmail.com>
2020-07-19 15:20:29 -0400

    Create list of unique binary search Tree

---
+++ src/com/challenge/dynamic_programming/Node.java
@@ -0,0 +1,15 @@
+package com.challenge.dynamic_programming;
+
+public class Node {
+	
+	int data;
+	
+	Node right, left;
+	
+	public Node(int data){
+		this.data = data;
+		right = null;
+		left = null;
+	}
+
+}
--- src/com/challenge/dynamic_programming/UniqueBinaryTrees.java
+++ src/com/challenge/dynamic_programming/UniqueBinaryTrees.java
@@ -1,5 +1,9 @@
 package com.challenge.dynamic_programming;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class UniqueBinaryTrees {
 	
 	private int countUniqueBinaryTrees(int n) {
@@ -19,11 +23,39 @@ public class UniqueBinaryTrees {
 		
 	}
 	
+	private List<Node> createUniqueBinaryTree(int start, int end){
+		List<Node> list = new ArrayList<Node>();
+		if(start>end) {
+			list.add(null);
+			return list;
+		}
+		
+		for(int i= start; i <= end; i++) {
+			List<Node> leftList = createUniqueBinaryTree(start, i-1);
+			List<Node> rightList = createUniqueBinaryTree(i+1, end);
+			
+			for(Node left: leftList) {
+				for(Node right: rightList) {
+					Node root = new Node(i);
+					root.left = left;
+					root.right = right;
+					list.add(root);
+				}
+			}
+		}
+		
+		return list;
+		
+	}
+	
 	public static void main(String[] args) {
 		UniqueBinaryTrees t = new UniqueBinaryTrees();
 		
 		int count = t.countUniqueBinaryTrees(3);
 		System.out.println(count);
+		
+		List<Node> list = t.createUniqueBinaryTree(1, 3);
+		System.out.println(list);
 	}
 
 }

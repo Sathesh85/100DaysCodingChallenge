2fe2f3a282ec5fb8e936d30cab8a9660bb9ee462
sathesh <satheshkumarv85@gmail.com>
2020-07-24 23:52:02 -0400

    Browsing history - Edit

--- src/com/challenge/system_design/BrowsingHistory.java
+++ src/com/challenge/system_design/BrowsingHistory.java
@@ -1,5 +1,8 @@
 package com.challenge.system_design;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class BrowsingHistory {
@@ -15,6 +18,9 @@ public class BrowsingHistory {
 		h.visit("twitter.com");
 		System.out.println(h.back(10));
 		System.out.println(h.forward(10));
+		int[] nums = {1,2,3};
+		
+		System.out.println(h.permute(nums));
 	}
     
     Stack<String> s1,s2;
@@ -31,10 +37,10 @@ public class BrowsingHistory {
     }
     
     public String back(int steps) {
-        while(steps-- >0 && !s1.isEmpty()) {
+        while(steps-- >1 && s1.size()>1) {
         	s2.push(s1.pop());
         }
-        return (!s1.isEmpty()) ? s1.peek() : s2.peek();
+        return s1.peek();
     }
     
     public String forward(int steps) {
@@ -43,5 +49,41 @@ public class BrowsingHistory {
          }
     	 return (!s1.isEmpty()) ? s1.peek() : s2.peek();
         
+    }
+    
+ public List<List<Integer>> permute(int[] nums) {
+        
+        return permute(nums,nums.length-1);
+        
+    }
+    
+    List<List<Integer>> permute(int[] nums, int idx){
+        List<List<Integer>> masterList = new ArrayList<>();
+        if(idx < 0){
+            masterList.add(new ArrayList<>());
+        } else {
+        	int rem = nums[idx];
+            List<List<Integer>> moreSubset = permute(nums, idx-1);
+            
+            for(List<Integer> subSet : moreSubset){
+                List<List<Integer>> moreSet = new ArrayList<>();
+                for(int i =0; i<=subSet.size();i++){
+                	moreSet.add(createList(i,rem, subSet));
+                }
+                masterList.addAll(moreSet);
+            }
+            
+        }
+        
+        return masterList;
+    }
+    
+    List<Integer> createList(int idx, int val, List<Integer> sub){
+        List<Integer> list = new ArrayList<>();
+        list.addAll(sub.subList(0,idx));
+        list.add(val);
+        if(sub.size()>idx)
+        list.addAll(sub.subList(idx,sub.size()));
+        return list;
     }
 }

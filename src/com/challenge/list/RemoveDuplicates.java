5151a30c3131b9bb0603339222fa4567217e5131
sathesh <satheshkumarv85@gmail.com>
2020-07-28 10:11:05 -0400

    Delete duplicates - List

--- src/com/challenge/list/RemoveDuplicates.java
+++ src/com/challenge/list/RemoveDuplicates.java
@@ -35,5 +35,29 @@ public class RemoveDuplicates {
         return tempDup.next;
 		
 	}
+	
+	 public ListNode deleteDuplicates(ListNode head) {
+	        ListNode dup = new ListNode(0);
+	        ListNode tempDup = dup;
+	        
+	        while(head!=null && head.next !=null){
+	            if(head.val != head.next.val){
+	                dup.next = head;
+	                dup = dup.next;
+	                head = head.next;
+	            } else {
+	                dup.next = head;
+	                dup = dup.next;
+	                while(head!=null && head.next!=null && head.val == head.next.val){
+	                    head = head.next;
+	                }
+	                head = head.next;
+	            }
+	        }
+	        
+	        dup.next = head;
+	        return tempDup.next;
+	        
+	    }
 
 }
